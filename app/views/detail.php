76d49cd1a5b9fea18f4d7a0abd6aa39ad8c0e6b2
var-sama <vava.and.aa72@gmail.com>
2026-03-22 13:41:38 +0700

    finished landing-detail page UI

--- app/views/detail.php
+++ app/views/detail.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GPU Post Element</title>
+    <title>GPU Post Detail</title>
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
     <link rel="stylesheet" href="/css/detail.css">
     <style>
